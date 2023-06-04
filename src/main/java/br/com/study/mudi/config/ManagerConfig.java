ec49ef721bbb058f27ce641d73b98f228e37ad7d
RickFerry <rick_ferry1@hotmail.com>
2023-06-04 17:21:53 -0300

    Screen logout

--- src/main/java/br/com/study/mudi/config/ManagerConfig.java
+++ src/main/java/br/com/study/mudi/config/ManagerConfig.java
@@ -16,7 +16,8 @@ public class ManagerConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated().and().formLogin(f -> f.loginPage("/login").permitAll());
+        http.authorizeRequests().anyRequest().authenticated().and().formLogin(f -> f.loginPage("/login").permitAll())
+                .logout(l -> l.logoutUrl("/logout"));
     }
 
     @Bean
