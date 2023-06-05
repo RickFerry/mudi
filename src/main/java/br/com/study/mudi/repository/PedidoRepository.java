8d0599dc4999b11af59acc8a3593a34acbdc3ace
RickFerry <rick_ferry1@hotmail.com>
2023-06-04 23:33:27 -0300

    Cache

--- src/main/java/br/com/study/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/study/mudi/repository/PedidoRepository.java
@@ -2,6 +2,7 @@ package br.com.study.mudi.repository;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,6 +13,7 @@ import br.com.study.mudi.model.StatusPedido;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 	
+	@Cacheable("pedidos")
 	List<Pedido> findByStatus(StatusPedido status, Pageable page);
 
 	@Query("select p from Pedido p join p.user u where u.username = :username")
