77cbb1f561b10121d462a62ac93d87d553813d53
RickFerry <rick_ferry1@hotmail.com>
2023-06-05 23:01:57 -0300

    Persistindo Oferta

--- src/main/java/br/com/study/mudi/api/OfertasRest.java
+++ src/main/java/br/com/study/mudi/api/OfertasRest.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class OfertasRest {
     private PedidoRepository pedidoRepository;
 
     @PostMapping
-    public Oferta criaOferta(OfertaDto dto) {
+    public Oferta criaOferta(@RequestBody OfertaDto dto) {
         Pedido p = pedidoRepository.findById(dto.getId())
                 .orElseThrow(() -> new EntityNotFoundException("Pedido não encontrado"));
         
